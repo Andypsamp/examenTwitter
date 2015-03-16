4d98a062329d8b3e3270b3b44304063dde20d305
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-16 08:54:32 +0100

    creamos el metodo q nos dara acceso a nuestra cuenta d twitter para poder trabajar con ella

--- src/codexamen/MetodosTwitter.java
+++ src/codexamen/MetodosTwitter.java
@@ -6,6 +6,7 @@
 package codexamen;
 
 import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
@@ -16,6 +17,19 @@ public class MetodosTwitter {
       ConfigurationBuilder cb = new ConfigurationBuilder();
     Twitter twitter;
     
+    
+    /**
+     * Constructor, da acceso a nuestra cuenta en twitter.
+     */
+    public MetodosTwitter() {
+
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("x3XClfqBRGr8XukF9lbjNcPAc")
+                .setOAuthConsumerSecret("gLSWYMXlKL9FRxogCD1UcVeAMEkY8xNnwPFwyE70fefssNr39g")
+                .setOAuthAccessToken("3055128525-94ajQH85l8EH02fUPSoSDgPfBamMreiWZoGN7TL")
+                .setOAuthAccessTokenSecret("FpVcQuvLobDT1cEPJ8SvFZAhcj807JV4HJ0yZXIOU7zKF");
+        twitter = new TwitterFactory(cb.build()).getInstance();
+    }
     
     
 }
