f27e69dadff5e49b9c6fc146e38f930aef88ba00
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-16 08:45:00 +0100

    Inicio de mi aplicacion de twitter

---
+++ src/codexamen/Aplicacion.java
@@ -0,0 +1,16 @@
+package codexamen;
+
+/**
+ *
+ * @author ANDY PEÑA SAMPEDRO
+ */
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
