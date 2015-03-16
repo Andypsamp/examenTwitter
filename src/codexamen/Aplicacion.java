8eff8ec7cb0f5d3948c6d8b157fafc88c748df66
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-16 09:12:23 +0100

    creamos el menu de la aplicacion

--- src/codexamen/Aplicacion.java
+++ src/codexamen/Aplicacion.java
@@ -1,5 +1,7 @@
 package codexamen;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author ANDY PEÑA SAMPEDRO
@@ -10,7 +12,26 @@ public class Aplicacion {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+  MetodosTwitter twit = new MetodosTwitter();
+        int op;
+        do {
+            op = Integer.parseInt(JOptionPane.showInputDialog("opciones: \n1 --> TimeLine\n2 --> Buscar\n3 --> Tweettear\n4 --> Exit"));
+            switch (op) {
+                case 1:
+                    twit.timeLine();
+                    break;
+                case 2:
+                    twit.buscar();
+                    break;
+                case 3:
+                    twit.tweet();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.exit(0);
+            }
+        } while (op != 0 && op != 4);
     }
-    
 }
