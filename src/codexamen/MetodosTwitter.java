f702680203f7967aadff58a65e0e5f0b0f5896f3
ANDY PEÑA SAMPEDRO <ANDY PEA SAMPEDRO@ANDY>
2015-03-16 08:59:45 +0100

    creamos el metodo buscar donde buscara twitter con una string introducida

--- src/codexamen/MetodosTwitter.java
+++ src/codexamen/MetodosTwitter.java
@@ -8,6 +8,9 @@ package codexamen;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import twitter4j.Query;
+import twitter4j.QueryResult;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -52,7 +55,23 @@ public class MetodosTwitter {
             Logger.getLogger(MetodosTwitter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+ /**
+     * Busca tweets con el string introducido
+     */
+    public void buscar() {
 
+        try {
+
+            String preg = JOptionPane.showInputDialog("Buscar:");
+            Query query = new Query(preg);
+            QueryResult result = twitter.search(query);
+            for (Status status : result.getTweets()) {
+                System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+            }
+        } catch (TwitterException ex) {
+            Logger.getLogger(MetodosTwitter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 
 
